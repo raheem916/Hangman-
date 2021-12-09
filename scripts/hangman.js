74256cf8ae6e11df50e3db53519d7f21cd783c31
raheem916 <92947997+raheem916@users.noreply.github.com>
2021-12-09 21:07:52 +0000

    answer line and letter selector

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,1 +1,40 @@
-console.log('test');
+let word = 'raheem';
+
+let allLetters = document.querySelectorAll('.alphabetContainer .letter');
+
+let guessContainer = document.querySelector('.guessContainer');
+//console.log(allLetters);
+let guessLetterHTML = '<div class="guessLetter"></div>';
+for(counter=0; counter < word.length; counter++){
+guessContainer.insertAdjacentHTML( 'beforeend', guessLetterHTML );
+}
+
+let guessLetters=document.querySelectorAll('.guessLetter');
+
+allLetters.forEach(function(letter){
+    //console.log(letter);
+    letter.addEventListener('click',function(event){
+        let currentLetterHTML = event.target;
+        if(!currentLetterHTML.classList.contains('clicked')){
+            let currentLetter = event.target.innerHTML.toLowerCase();
+            console.log(currentLetter);
+            currentLetterHTML.classList.add('clicked');
+            let answers =[...word.matchAll(currentLetter)];
+            //console.log(answers);
+            if(answers){
+                answers.forEach(function(answer){
+                    //console.log(answer.index);
+                    for(counter=0; counter < guessLetters . length;counter++){
+                        if(counter == answer.index){
+                            //console.log('Correct Letter');
+                             guessLetters[answer.index].innerHTML = currentLetter.toUpperCase();
+
+                     }
+                    }
+                })
+            }else{
+                //showthe next part of the hangman
+            }    
+        }
+    });
+});
